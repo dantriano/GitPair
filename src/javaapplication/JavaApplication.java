dc7c674bfb278de1d4fa7e46f56c3d2fb9a58620
Dan <dan.triano@gmail.com>
2020-12-14 01:19:12 +0100

    GitPair

---
+++ src/javaapplication/JavaApplication.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication;
+
+/**
+ *
+ * El programador A rellenará el main para hacer un programa que pregunte 
+ * la edad y el nombre a una persona por teclado y calcule su tiempo de vida en
+ *  dias, horas , minutos y segundos con la frase:
+ * 
+ * Felicidades [nombre] has vivido:
+ * x dias
+ * xx horas
+ * xxxx minutos
+ * xxxxxxxxx segundos
+ * 
+ * @author Pon tu nombre programador A
+ */
+public class JavaApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*Inicializamos las variables necesarias*/
+        //
+        //
+        
+        
+        /* Preguntamos el nombre del usuario*/
+        // nombre=
+       
+        /* Preguntamos la edad en años usuario*/
+        // edad=
+        
+        /*Aqui calculamos los dias*/
+        //dias=
+        
+        /*Aqui calculamos las horas*/
+        //horas=
+        
+        /*Aqui calculamos los minutos*/
+        //minutos=
+        
+        /*Aqui calculamos los segundos*/
+        //segundos=
+        
+        /*Salida por pantalla*/
+        //System.out.println();
+    }
+
+}
